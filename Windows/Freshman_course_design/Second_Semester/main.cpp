2fd9c52143d329162ad494803c6baaee1d961c93
Iridescent-9 <1036220419@qq.com>
2020-06-10 20:34:38 +0800

    增加一点说明

--- Windows/Freshman_course_design/Second_Semester/main.cpp
+++ Windows/Freshman_course_design/Second_Semester/main.cpp
@@ -1,4 +1,9 @@
-﻿//克隆至本地后请先修改manage.h中void manage::info ( car* info )函数打开文件的路径
+﻿/*克隆至本地后请先修改manage.h中
+void manage::info_read ( car* info )和void manage::info_write(car*info)函数打开文件的路径
+由于是用UTF-8编码格式写的
+克隆至本地后可能会出现乱码
+自己想办法了，编码格式UTF-8情况下不能输入中文
+GBK和GB2312情况下可以，这个bug我也很懵逼*/
 #include<iostream>
 #include<string.h>
 #include<string>
@@ -16,11 +21,9 @@ int main ( )
 	Sleep ( 2000 );
 	return 0;
 }
-
-
 //==================================================//
-//            Copyright@ Han 2020                   //
+//            Copyright© Han 2020                   //
 //            Author:    Han                        //
 //            Email:     syhan1228@vip.qq.com       //
-//            Time:      2020-06-02 19:03:26        //
+//            Time:      2020-06-10  20:32:55       //
 //==================================================//
