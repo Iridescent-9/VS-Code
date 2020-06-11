de79419f53b5392ab6889305e7a92a3d1b67f0d2
Iridescent-9 <1036220419@qq.com>
2020-06-11 09:05:56 +0800

    尝试新GCC

--- test.cpp
+++ test.cpp
@@ -1,41 +1,10 @@
 #include <iostream>
-#include <string.h>
-#include <string>
-#include <fstream>
+
 using namespace std;
+
 int main()
 {
-    int set(string test);
-    int a;
-    string b;
-start:
-{
-    cin >> b;
-    if (set(b) != 0)
-    {
-        a = set(b);
-    }
-    else
-    {
-        cout << "请重新输入";
-        goto start;
-    }
-}
-    cout<<a;
+    cout<<"hello world"<<endl;
     getchar();
     return 0;
-}
-int set(string test)
-{
-    int x;
-    if (test != "1"&& test!="2")
-    {
-        cout << "违法输入" << endl;
-        return 0;
-    }
-    else
-    {
-        x = stoi(test, 0, 10);
-        return x;
-    }
 }
