baa775dea0d67cc45d7112a0c13788934a6b5ba0
Iridescent-9 <61072544+Iridescent-9@users.noreply.github.com>
2021-01-26 22:27:49 +0800

    w

---
+++ Windows/Sophomre_course_design/First_Semester/main.cpp
@@ -0,0 +1,21 @@
+//File's name:main.cpp
+//File's path:D:\VSCode\C++\main.cpp
+//==================================================//
+//            Author:          Iridescent-9         //
+//            Email:           syhan1228@vip.qq.com //
+//            Creat Time:      2020-12-18  23:02:21 //
+//==================================================//
+#include <iostream>
+#include <windows.h>
+#include "function.h"
+using namespace std;
+
+int main ( )
+{
+    system ( "chcp 65001" );
+    system ( "cls" );
+    menu ( );
+    getchar ( );
+    getchar ( );
+    return 0;
+}
